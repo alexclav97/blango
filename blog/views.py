b4e59f51bbda6c2377af112bf377d776b75b4980
Alexander Ledesma <aclavijo97@gmail.com>
2023-10-05 21:57:56 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
-def index(request):
+def index(request):  
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
-def post_detail(request, slug):
+def post_detail(request, slug):  
   post = get_object_or_404(Post, slug=slug)    
   if request.user.is_active:
       if request.method == "POST":
